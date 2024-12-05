32104b8fcbac934b19e18d8ece23a00d367cf9aa
LightedExe <Thundermagicreal@gmail.com>
2024-12-05 09:43:54 +0800

    Automatically create and open docx file

--- Reporter.ps1
+++ Reporter.ps1
@@ -1,1 +1,15 @@
+# Open shell for script
 Start-Process cmd -ArgumentList "/k", "poetry shell"
+
+# Get docx file path
+$downloads = [Environment]::GetFolderPath("MyDocuments").Replace("Documents", "Downloads")
+$date = Get-Date -Format "ddMMyyyy"
+$filePath = "$downloads\MITRE ATT Report $date - faisal.docx"
+
+# Create file if it doesnt exist
+if (-not (Test-Path -Path $filePath)) {
+    New-Item -Path $filePath -ItemType File
+}
+
+# Open the file after creation
+Start-Process $filepath
